450fa2d43cbab74b480006f5758e98d4c92ba757
Dhruw <dharmendra23101@iiitnr.edu.in>
2025-09-12 15:27:06 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,6 +9,7 @@ const pool = new Pool({
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT || 5432,
+  ssl: { rejectUnauthorized: false }, // <--- Required for Supabase
 });
 
 // Test database connection
